51f35e608ffd102481c913577cf045da982ed067
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-04-04 21:52:10 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,7 +9,8 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    binding.pry
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
